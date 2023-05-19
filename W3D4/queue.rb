f9c7a463b4145d7a8a7ece8ea67ddd83f4e8e5f7
XLG123 <xiaolin.guan123@gmail.com>
2023-05-19 10:24:52 -0400

    updated MyQueue Class

--- W3D4/queue.rb
+++ W3D4/queue.rb
@@ -1,22 +1,35 @@
-class Queue
+class MyQueue
   def initialize
-    @ivar = []
+    @queue = []
   end
 
   def enqueue(el)
-    @ivar << el
+    queue << el
+    return true
   end
 
   def dequeue
-    @ivar.shift
+    queue.shift
   end
 
   def peek
-    @ivar.first
+    queue.first
   end
+
+  def empty?
+    queue.empty?
+  end
+  
+  # create a shallow copy of the queue to limit user access
+  def show
+    queue.dup 
+  end
+
+  private
+  attr_reader :queue
 end
 
-queue_1 = Queue.new
+queue_1 = MyQueue.new
 queue_1.enqueue("KKK")
 p queue_1.peek
 queue_1.enqueue("KYK")
@@ -30,3 +43,15 @@ puts
 
 queue_1.dequeue
 p queue_1.peek
+
+puts
+
+p queue_1.empty?
+
+puts
+
+p queue_1.show
+
+p queue_1.show.reverse!
+
+p queue_1.show

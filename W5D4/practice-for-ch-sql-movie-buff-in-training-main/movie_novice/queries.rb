3ac3b15b0bfecc308cbbbffe6d87768a1ed39173
XLG123 <xiaolin.guan123@gmail.com>
2023-06-11 23:03:49 -0400

    W7D1 hw in progress

--- W5D4/practice-for-ch-sql-movie-buff-in-training-main/movie_novice/queries.rb
+++ W5D4/practice-for-ch-sql-movie-buff-in-training-main/movie_novice/queries.rb
@@ -54,7 +54,7 @@ def below_average_years
   # display each year with movies scoring under 5, with the count of movies
   # scoring under 5 aliased as bad_movies, in descending order
   # hint: use 'select', 'where', 'group', 'order'
-  Movie
+  subset1 = Movie
     .select('yr', 'COUNT(*) as bad_movies')
     .where('score < 5')
     .group('yr')
